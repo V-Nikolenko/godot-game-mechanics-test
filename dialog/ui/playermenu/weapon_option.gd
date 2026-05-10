280c6c116f50cc93b1a53128ac6e819439ee08f4
Nikolenko Vladislav <48482332+V-Nikolenko@users.noreply.github.com>
2026-05-10 17:14:50 +0300

    refactor: remove mouse click from WeaponOption, add keyboard cursor visual

--- dialog/ui/playermenu/weapon_option.gd
+++ dialog/ui/playermenu/weapon_option.gd
@@ -2,18 +2,16 @@
 class_name WeaponOption
 extends Control
 
-signal option_pressed
-
 const _TEX_UNSELECTED: Texture2D = preload("res://assault/assets/sprites/ui/weapon_list_item_select_option.png")
 const _TEX_SELECTED: Texture2D = preload("res://assault/assets/sprites/ui/weapon_list_item_selected_option.png")
+
+## Colour applied to this control when the keyboard cursor is on it.
+const _CURSOR_MODULATE := Color(1.4, 1.4, 1.0)
+const _NORMAL_MODULATE := Color.WHITE
 
 @onready var _icon_sprite: Sprite2D = $WeaponIcon
 @onready var _bg_sprite: Sprite2D = $SelectionBG
 @onready var _label: Label = $WeaponName
-@onready var _button: Button = $ClickArea
-
-func _ready() -> void:
-	_button.pressed.connect(option_pressed.emit)
 
 ## Set the display name and icon texture. Call this after instantiating the scene.
 func configure(display_name: String, icon: Texture2D) -> void:
@@ -27,3 +25,7 @@ func set_selected(value: bool) -> void:
 	if _bg_sprite.texture == tex:
 		return
 	_bg_sprite.texture = tex
+
+## Highlight this row when the keyboard cursor is positioned here.
+func set_cursor(value: bool) -> void:
+	modulate = _CURSOR_MODULATE if value else _NORMAL_MODULATE
